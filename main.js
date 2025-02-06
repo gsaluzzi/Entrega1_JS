6270465c9e4b88025b7441e343b7ff4718513c4a
Giorgio Saluzzi <giosaluzzi@gmail.com>
2025-02-06 09:41:32 -0300

    Detalles finales

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ function verSocios() {
 function agregarSocio(){
     const nuevoSocio = prompt("Ingrese el nombre del Nuevo Socio");
     
-    if(nuevoSocio != null){
+    if (nuevoSocio == false){
+        alert("Debes agregar un nombre válido.\nIntentalo nuevamente. Volverás a la pantalla de Inicio");
+    }
+    else if(nuevoSocio != null){
         listaDeJugadoresSocios.push(nuevoSocio);
-        alert("Agregaste a la lista de Socios a " + nuevoSocio);
+        alert("Felicitaciones, agregaste a la lista de Socios a " + nuevoSocio);
     } else {
         alert("Ingresa un nombre válido");
     };
@@ -35,20 +38,24 @@ function verInscritos() {
 
 
 function agregarInscrito(){
-    const nuevoInscrito = prompt("Ingrese el nombre del Socio a Inscribir");
-        if(listaDeJugadoresSocios.includes(nuevoInscrito)){
+    const nuevoInscrito = prompt("Ingrese el nombre del Socio a Inscribir en el Campeonato");
+        if(nuevoInscrito == false){
+        alert("Ingresa un nombre válido");
+    } else if(listaDeJugadoresSocios.includes(nuevoInscrito)){
         InscritosCampeonato.push(nuevoInscrito);
         alert("Inscribiste en el campeonato al Socio " + nuevoInscrito);
+    } else if(nuevoInscrito == null){
+        alert("Ingresa un nombre válido");
     } else {
-        alert("Primero debes Agregar como Socio a " + nuevoInscrito + " y luego inscribirlo");
-    };
+        alert("Primero debes Agregar como Socio a " + nuevoInscrito + " y luego podrás inscribirlo");
+    }
 };
 
 
 
 
 do {
-    tipo = parseInt(prompt("Bienvenido al sistema de inscripción Campeonato de Tenis CODERHOUSE 2025\n\n1. Para ver la lista de Socios.\n2. Para agregar un Socio.\n3. Para ver los jugadores ya inscritos.\n4. Para Inscribir a un Socio en el Torneo.\n \nPara salir ingrese 0."));
+    tipo = parseInt(prompt("Bienvenido al sistema de inscripción Campeonato de Tenis CODERHOUSE 2025. Recuerda que solo los Socios pueden inscribirse en el Campeonato.\n\n1. Para ver la lista de Socios.\n2. Para agregar un Socio.\n3. Para ver los jugadores ya inscritos en el Campeonato.\n4. Para Inscribir a un Socio en el Campeonato.\n \nPara salir ingrese 0."));
 
     switch (tipo) {
         case 0:
